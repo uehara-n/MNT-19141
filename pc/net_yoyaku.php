4a40f7665449ff9c2bdbeb9fd80dc881233a424f
uehara-n <na.ue0426@gmail.com>
2018-08-05 11:51:09 +0900

    add new file

---
+++ pc/net_yoyaku.php
@@ -0,0 +1,39 @@
+<div id="raiten_page">
+<p class="top_img"><img src="http://www.mrdeiku.jp/wp-content/themes/reform/page_image/net_yoyaku/top_img1-1.png" alt="ネットでの来店予約がオススメ" width="719" height="400" /></p>
+
+<!-- ★★★★★
+<div class="white_layer">
+★★★★★ -->
+<div class="viewmap">
+<iframe src="https://www.google.com/maps/embed?pb=!4v1517365960380!6m8!1m7!1sCAoSLEFGMVFpcE9fVlYwdE9ldHRvdUxZMERVZnZhajBkdTZxZld2M0RpdjZpVkp4!2m2!1d35.63642412264766!2d138.5479891837375!3f3.39!4f-4.920000000000002!5f0.7820865974627469" width="719" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+</div>
+<p class="top_img"><img src="http://www.mrdeiku.jp/wp-content/themes/reform/page_image/net_yoyaku/top_img1-2.png" alt="初回はプレゼントもございますので、ぜひご活用ください♪" width="719" height="414" /></p>
+				<h3>ご来店予約からの流れ</h3>
+				<p class="nagare"><img src="http://www.mrdeiku.jp/wp-content/themes/reform/page_image/net_yoyaku/top_img02.png" /></p>
+				<div class="cap">
+<img src="http://www.mrdeiku.jp/wp-content/themes/reform/page_image/net_yoyaku/net_yoyaku_03.jpg" width="57" alt="ご注意" /><br />
+				<ul>
+					<li>QUOカードのプレゼントは、初めてご来店される方が対象です。1組様につき1回限りとさせていただきます。<br />
+						※プレゼントは予告なく変更・廃止することがございます。</li>
+					<li>未成年者様のみのご来場は対象外とさせていただきます。</li>
+					<li>ご来場後、アンケートにご協力していただく事が条件となります。</li>
+					<li>当社からの予約確認、確定の連絡を以て受付完了とさせていただきます。<br />
+						また、前日・当日のご来店予約につきましては、直接お電話にてご連絡いただきますよう、ご協力お願い致します。</li>
+					<li>ご入力いただいた個人情報は、弊社プライバシーポリシーに基づき管理いたします。</li>
+				</ul>
+				</div>
+
+<div class="form mb20">
+<img src="http://www.mrdeiku.jp/wp-content/themes/reform/page_image/net_yoyaku/form_tit.gif" class="tit" width="710" height="56" />
+<?php if (wp_is_mobile()) :?>
+[contact-form-7 id="17022" title="来店予約_スマホ版"]
+<?php else: ?>
+[contact-form-7 id="14603" title="来店予約"]
+<?php endif; ?>
+
+<!-- ★★★★★
+</div><!-- //.white_layer
+★★★★★ -->
+
+<!-- <a href="/contact"><img src="http://www.mrdeiku.jp/wp-content/themes/reform/page_image/net_yoyaku/b_image.png" width="719" height="145" /></a> -->
+</div>

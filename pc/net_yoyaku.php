10e3fc5e468ef73f7281398c41763bd31a0f63ae
uehara-n <na.ue0426@gmail.com>
2018-08-05 17:10:31 +0900

    net_yoyaku.page

--- pc/net_yoyaku.php
+++ pc/net_yoyaku.php
@@ -5,26 +5,54 @@
 <div class="white_layer">
 ★★★★★ -->
 <div class="viewmap">
-<iframe src="https://www.google.com/maps/embed?pb=!4v1517365960380!6m8!1m7!1sCAoSLEFGMVFpcE9fVlYwdE9ldHRvdUxZMERVZnZhajBkdTZxZld2M0RpdjZpVkp4!2m2!1d35.63642412264766!2d138.5479891837375!3f3.39!4f-4.920000000000002!5f0.7820865974627469" width="719" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+	<iframe src="https://www.google.com/maps/embed?pb=!4v1517365960380!6m8!1m7!1sCAoSLEFGMVFpcE9fVlYwdE9ldHRvdUxZMERVZnZhajBkdTZxZld2M0RpdjZpVkp4!2m2!1d35.63642412264766!2d138.5479891837375!3f3.39!4f-4.920000000000002!5f0.7820865974627469" width="719" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 </div>
-<p class="top_img"><img src="http://www.mrdeiku.jp/wp-content/themes/reform/page_image/net_yoyaku/top_img1-2.png" alt="初回はプレゼントもございますので、ぜひご活用ください♪" width="719" height="414" /></p>
-				<h3>ご来店予約からの流れ</h3>
-				<p class="nagare"><img src="http://www.mrdeiku.jp/wp-content/themes/reform/page_image/net_yoyaku/top_img02.png" /></p>
-				<div class="cap">
-<img src="http://www.mrdeiku.jp/wp-content/themes/reform/page_image/net_yoyaku/net_yoyaku_03.jpg" width="57" alt="ご注意" /><br />
-				<ul>
-					<li>QUOカードのプレゼントは、初めてご来店される方が対象です。1組様につき1回限りとさせていただきます。<br />
-						※プレゼントは予告なく変更・廃止することがございます。</li>
-					<li>未成年者様のみのご来場は対象外とさせていただきます。</li>
-					<li>ご来場後、アンケートにご協力していただく事が条件となります。</li>
-					<li>当社からの予約確認、確定の連絡を以て受付完了とさせていただきます。<br />
-						また、前日・当日のご来店予約につきましては、直接お電話にてご連絡いただきますよう、ご協力お願い致します。</li>
-					<li>ご入力いただいた個人情報は、弊社プライバシーポリシーに基づき管理いたします。</li>
-				</ul>
-				</div>
+
+
+<img class="bnr" src="http://www.mrdeiku.jp/wp-content/themes/reform/images/common/bnr_quo.jpg" />
+<h3 class="5merit_tit">ご来店のお客様に<span>5つ</span>のメリット</h3>
+
+<div class="wrap_5merit">
+	<div class="inbox">
+		<!-- <p class="title"><span class="number">01</span>待ち時間なしの<br>ご案内</p> -->
+		<img src="http://www.mrdeiku.jp/wp-content/themes/reform/page_image/net_yoyaku/5merit.jpg">
+		<p>ご予約制なので、予約された時間に担当のスタッフがしっかりと応対！お待ちいただく時間無しで、ストレスなく、リフォームの相談をできます！</p>
+	</div>
+	<div class="inbox">
+		<!-- <p class="title"><span class="number">02</span>住宅設備体感</p> -->
+		<img src="http://www.mrdeiku.jp/wp-content/themes/reform/page_image/net_yoyaku/5merit02.jpg" alt="住宅設備体感">
+		<p>TOTO、LIXIL、Panasonic など一流メーカーの最新システムキッチンやユニットバス。最新の住宅設備を触って確かめる事ができます。</p>
+	</div>
+	<div class="inbox">
+		<!-- <p class="title"><span class="number">03</span>助成金の<br>アドバイス</p> -->
+		<img src="http://www.mrdeiku.jp/wp-content/themes/reform/page_image/net_yoyaku/5merit03.jpg">
+		<p>リフォームで一番不安なことは「費用の不安」という方が多いのではないでしょうか。各市区町村ごとで使用可能な｢助成金｣が活用出来るかもしれません。サンプロでは助成金に詳しいスタッフも多数在籍していますので、お気軽にご相談ください｡</p>
+	</div>
+	<div class="inbox">
+		<!-- <p class="title"><span class="number">04</span>助成金の<br>アドバイス</p> -->
+		<img src="http://www.mrdeiku.jp/wp-content/themes/reform/page_image/net_yoyaku/5merit04.jpg">
+		<p>リフォームで一番不安なことは「費用の不安」という方が多いのではないでしょうか。各市区町村ごとで使用可能な｢助成金｣が活用出来るかもしれません。サンプロでは助成金に詳しいスタッフも多数在籍していますので、お気軽にご相談ください｡</p>
+	</div>
+	<div class="inbox">
+		<!-- <p class="title"><span class="number">05</span>助成金の<br>アドバイス</p> -->
+		<img src="http://www.mrdeiku.jp/wp-content/themes/reform/page_image/net_yoyaku/5merit05.jpg">
+		<p>リフォームで一番不安なことは「費用の不安」という方が多いのではないでしょうか。各市区町村ごとで使用可能な｢助成金｣が活用出来るかもしれません。サンプロでは助成金に詳しいスタッフも多数在籍していますので、お気軽にご相談ください｡</p>
+	</div>
+</div>
+
+<div class="wrap_cbnr">
+	<img class="bnr" src="http://www.mrdeiku.jp/wp-content/themes/reform/images/common/bnr_quo.jpg" />
+	<img class="icon01" src="http://www.mrdeiku.jp/wp-content/themes/reform/images/common/ico_tel.png" class="ico_file" />
+	<h4 class="net_yoyaku">お電話でのお問い合わせ</h3>
+	<a href="contact"><img class="bnr" src="http://www.mrdeiku.jp/wp-content/themes/reform/images/common/contact_bnr.png" class="050-7300-0827" /></a>
+
+	<img class="icon01" src="http://www.mrdeiku.jp/wp-content/themes/reform/images/common/ico_file.png" class="ico_file" />
+	<h4 class="net_yoyaku">メールでのお問い合わせ</h3>
+</div>
 
 <div class="form mb20">
-<img src="http://www.mrdeiku.jp/wp-content/themes/reform/page_image/net_yoyaku/form_tit.gif" class="tit" width="710" height="56" />
+	<p>※お問い合わせ内容を下記フォームに入力、送信して下さい。</p>
+<!-- <img src="http://www.mrdeiku.jp/wp-content/themes/reform/page_image/net_yoyaku/form_tit.gif" class="tit" width="710" height="56" /> -->
 <?php if (wp_is_mobile()) :?>
 [contact-form-7 id="17022" title="来店予約_スマホ版"]
 <?php else: ?>
